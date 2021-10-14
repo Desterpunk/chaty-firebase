9097858a3b7653c767322e32d003f3b351b138c2
Jhon Serna <jhonalfaber@gmail.com>
2021-10-13 21:46:46 -0500

    Creacion del home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class HomePage extends Component {
   render() {
     return (
       <div>
-          HomePage
+          <section>
+            <div>
+              <div>
+                <h1>Welcome to Chatty</h1>
+                <p>A great place to share your thoughts with friends</p>
+              </div>
+              <div>
+                <Link to = "/signup">Create New Account</Link>
+                <Link to = "/login">Login to your Account</Link>
+              </div>
+            </div>
+          </section>
       </div>
     )
   }
